6f2975bd937ea6ce0391b709b2c58a02a61cd667
Dominika Florczykowska <dflorczy@protonmail.com>
2021-12-04 19:13:15 +0100

    Improve day 2 part 1

--- day2/day2_part1.sql
+++ day2/day2_part1.sql
@@ -1,29 +1,16 @@
-CREATE TABLE IF NOT EXISTS commands(id int, direction varchar(255), unit int);
-
-WITH direction_sums AS (
-	SELECT 
-		direction,
-		SUM(unit) as total
-	FROM commands
-	GROUP BY direction
-),
-final_position AS(
-	SELECT
-		SUM
-		(CASE
-			WHEN direction = 'down' THEN total
-			WHEN direction = 'up' THEN total * -1
-			ELSE 0
-			END
-		) AS vertical,
-		SUM
-		(CASE
-			WHEN direction = 'forward' THEN total
-			ELSE 0
-			END
-		) AS horizontal
-	FROM direction_sums
-)
 SELECT
-	vertical * horizontal
-FROM final_position
+	SUM
+	(CASE
+		WHEN direction = 'down' THEN unit
+		WHEN direction = 'up' THEN unit * -1
+		ELSE 0
+		END
+	)
+	*
+	SUM
+	(CASE
+		WHEN direction = 'forward' THEN unit
+		ELSE 0
+		END
+	)
+FROM commands
