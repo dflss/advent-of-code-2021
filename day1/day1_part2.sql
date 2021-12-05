0c83eab99c4b9d9f4c997a0ff74bdb789544a258
Dominika Florczykowska <dflorczy@protonmail.com>
2021-12-05 17:24:14 +0100

    Lint with sqlfluff

--- day1/day1_part2.sql
+++ day1/day1_part2.sql
@@ -1,21 +1,21 @@
 CREATE TABLE IF NOT EXISTS measurements(id int, depth int);
 
-WITH 
+WITH
 measurements_with_moving_sum AS (
-    SELECT 
-	id,
-	depth,
-	SUM(depth) OVER(ORDER BY id
-			ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING )
-			AS moving_sum
+    SELECT
+        id,
+        depth,
+        SUM(depth) OVER(ORDER BY id
+            ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING )
+        AS moving_sum
     FROM measurements
 ),
+
 measurements_with_deltas AS (
-    SELECT 
-	moving_sum - LAG(moving_sum) OVER(ORDER BY id) as delta
+    SELECT moving_sum - LAG(moving_sum) OVER(ORDER BY id) AS delta
     FROM measurements_with_moving_sum
 )
-SELECT 
-    COUNT(delta) AS increasing_rows_count
+
+SELECT COUNT(delta) AS increasing_rows_count
 FROM measurements_with_deltas
 WHERE delta > 0
--- day2/day2_part1.sql
+++ day2/day2_part1.sql
@@ -1,18 +1,13 @@
 CREATE TABLE IF NOT EXISTS commands(id int, direction varchar(255), unit int);
 
-SELECT
-	SUM
-	(CASE
-		WHEN direction = 'down' THEN unit
-		WHEN direction = 'up' THEN unit * -1
-		ELSE 0
-		END
-	)
-	*
-	SUM
-	(CASE
-		WHEN direction = 'forward' THEN unit
-		ELSE 0
-		END
-	)
+SELECT SUM(CASE
+        WHEN direction = 'down' THEN unit
+        WHEN direction = 'up' THEN unit * -1
+        ELSE 0
+        END
+    ) * SUM(CASE
+        WHEN direction = 'forward' THEN unit
+        ELSE 0
+        END
+    )
 FROM commands
